4ec4b4012a1f0e94e0a9fcb1ed67e05d1756e1c3
pragnakasthruri <kpragna25@gmail.com>
2022-02-22 11:58:22 +0530

    Welcome to Line Comaprison Compuation

---
+++ src/com/bridgelabz/LineComparison.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+/**
+ * Start with Displaying
+ * Welcome to LineComparison Computation Program 
+ */
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
